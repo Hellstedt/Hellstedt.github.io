1c1bc4ff3999bfffb15eeabf7389c13a6445f7ff
Joakim Hellstedt <joakim.v.hellstedt@gmail.com>
2024-05-04 21:42:30 +0200

    Enabled hidden nav-bar

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// Enable hidden nav bar
+
+const nav = document.querySelector(".nav");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if(lastScrollY < window.scrollY) {
+        nav.classList.add("nav-hidden");
+    } else {
+        nav.classList.remove("nav-hidden");
+    }
+
+    lastScrollY = window.scrollY;
+});
+
+// Image carousel in projcet cards
 document.querySelectorAll(".card-carousel").forEach(carousel => {
     const items = carousel.querySelectorAll(".carousel-item");
     const buttonsHtml = Array.from(items, () => {
